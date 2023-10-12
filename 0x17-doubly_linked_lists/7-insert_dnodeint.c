6d99f613b623982d747d429793bfbd95b3d09e13
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-10-12 12:04:08 -0700

    chng

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	for (i = 0; temp && i < idx; i++)
 	{
-		if (i == idx -1)
+		if (i == idx - 1)
 		{
 			if (temp->next == NULL)
 				return (add_dnodeint_end(h, n));
@@ -40,5 +40,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 	}
 
-	return(NULL);
+	return (NULL);
 }
